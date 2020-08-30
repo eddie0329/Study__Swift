2ca4bd12d575aac7447bcbbc900075a43cb6b3b2
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-08-30 23:49:22 +0900

    :tada: Init calculator project

---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by 최종선 on 2020/08/30.
+//  Copyright © 2020 최종선. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
