593b8cdc1d5219925a66de4ad111c59cd840f924
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-09-02 22:13:40 +0900

    :sparkles: Finish Ch1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+print("HELLO");
+print("Hi iam Eddie");
+print("hello world");
+
+
+// let is constant
+let b = 10;
+
+// var is variable
+var a = 5;
+
+a = b
+
+print("a = ", a);
+print("b = ", b);
+
+let (x, y) = (1, 2);
+print("x = ", x);
+print("y = ", y);
+
+var num = 1;
+while num < 8 {
+    num+=1;
+}
---
+++ ch02_const_var_tuple.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let constNum = 100;
+var varNum = 200;
+
+print("constNum = \(constNum)");
+varNum = 300;
+print("varNum = \(varNum)");
