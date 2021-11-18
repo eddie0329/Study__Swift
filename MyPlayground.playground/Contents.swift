7237f3a3d43e625c6dfcd32dcf27394614c1d18d
eddie0329 <fdsa0329@naver.com>
2021-11-18 23:41:16 +0900

    :sparkles: Start Swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,26 +1,5 @@
 import UIKit
 
-print("HELLO");
-print("Hi iam Eddie");
-print("hello world");
+var str = "Hello, playground"
 
-
-// let is constant
-let b = 10;
-
-// var is variable
-var a = 5;
-
-a = b
-
-print("a = ", a);
-print("b = ", b);
-
-let (x, y) = (1, 2);
-print("x = ", x);
-print("y = ", y);
-
-var num = 1;
-while num < 8 {
-    num+=1;
-}
+print(str);
